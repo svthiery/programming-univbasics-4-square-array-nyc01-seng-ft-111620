19be8bebf8f8c8a347843c40158d057b2854b13a
Sarah Kain Thiery <sarahvkain@gmail.com>
2020-10-26 14:44:45 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,6 +2,9 @@ def square_array(array)
   counter = 0 
   
   while counter < array.length do
-    
+    array[counter] = array[counter] ** array[counter]
+    counter += 1 
   end
+  
+  array 
 end
